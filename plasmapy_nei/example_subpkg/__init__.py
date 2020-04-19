2d1b430a63e06cd474687bc70c7c9a347cad63f7
Nick Murphy <namurphy@cfa.harvard.edu>
2020-04-18 23:16:22 -0400

    Make sure pep8speaks is working

--- plasmapy_nei/example_subpkg/__init__.py
+++ plasmapy_nei/example_subpkg/__init__.py
@@ -3,3 +3,8 @@ This is the docstring for the examplesubpkg package.  Normally you would
 have whatever.py files in this directory implementing some modules, but this
 is just an example sub-package, so it doesn't actually do anything.
 """
+
+import numpy as np
+
+def purposeful_pep8_error():
+    print("To test pep8speaks on GitHub")
