0dd4179b857b2b90bbf974ce8bdf2f9433c4d064
Nick Murphy <namurphy@cfa.harvard.edu>
2022-03-17 18:31:37 -0400

    Temporarily remove __version__ from __init__.py

--- plasmapy_nei/__init__.py
+++ plasmapy_nei/__init__.py
@@ -2,13 +2,4 @@
 
 __all__ = ["eigen", "nei"]
 
-import warnings
-
-try:
-    from plasmapy_nei.version import __version__
-except Exception:
-    warnings.warn("Unable to import __version__")
-finally:
-    del warnings
-
 from plasmapy_nei import eigen, nei
