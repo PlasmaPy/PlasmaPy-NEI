fe11c202c071ad7e9208d8f20a088dae5c9df840
Nick Murphy <namurphy@cfa.harvard.edu>
2022-03-17 18:45:44 -0400

    Copy over version stuff from PlasmaPy

--- plasmapy_nei/__init__.py
+++ plasmapy_nei/__init__.py
@@ -2,4 +2,49 @@
 
 __all__ = ["eigen", "nei"]
 
+import pkg_resources
+
 from plasmapy_nei import eigen, nei
+
+# define version
+try:
+    # this places a runtime dependency on setuptools
+    #
+    # note: if there's any distribution metadata in your source files, then this
+    #       will find a version based on those files.  Keep distribution metadata
+    #       out of your repository unless you've intentionally installed the package
+    #       as editable (e.g. `pip install -e {plasmapy_directory_root}`),
+    #       but then __version__ will not be updated with each commit, it is
+    #       frozen to the version at time of install.
+    #
+    #: PlasmaPy version string
+    __version__ = pkg_resources.get_distribution("plasmapy_nei").version
+except pkg_resources.DistributionNotFound:
+    # package is not installed
+    fallback_version = "unknown"
+    try:
+        # code most likely being used from source
+        # if setuptools_scm is installed then generate a version
+        from setuptools_scm import get_version
+
+        __version__ = get_version(
+            root="..", relative_to=__file__, fallback_version=fallback_version
+        )
+        del get_version
+        warn_add = "setuptools_scm failed to detect the version"
+    except ModuleNotFoundError:
+        # setuptools_scm is not installed
+        __version__ = fallback_version
+        warn_add = "setuptools_scm is not installed"
+
+    if __version__ == fallback_version:
+        from warnings import warn
+
+        warn(
+            f"plasmapy.__version__ not generated (set to 'unknown'), PlasmaPy is "
+            f"not an installed package and {warn_add}.",
+            RuntimeWarning,
+        )
+
+        del warn
+    del fallback_version, warn_add
