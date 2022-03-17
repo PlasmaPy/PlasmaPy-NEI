c3268158600ed41d3c689cd44dccc2575e407b17
Nick Murphy <namurphy@cfa.harvard.edu>
2022-03-17 18:39:28 -0400

    Remove relative import

--- plasmapy_nei/eigen/__init__.py
+++ plasmapy_nei/eigen/__init__.py
@@ -1,3 +1,3 @@
 """Classes for accessing eigentables for ionization and recombination rates."""
 
-from .eigenclass import eigen_data_dict, EigenData
+from plasmapy_nei.eigenclass import eigen_data_dict, EigenData
