f9442c2c35e0c15c6a77b6bc59bc630468a631df
Nick Murphy <namurphy@cfa.harvard.edu>
2022-03-17 18:41:20 -0400

    Fix import

--- plasmapy_nei/eigen/__init__.py
+++ plasmapy_nei/eigen/__init__.py
@@ -1,3 +1,3 @@
 """Classes for accessing eigentables for ionization and recombination rates."""
 
-from plasmapy_nei.eigenclass import eigen_data_dict, EigenData
+from plasmapy_nei.eigen.eigenclass import eigen_data_dict, EigenData
